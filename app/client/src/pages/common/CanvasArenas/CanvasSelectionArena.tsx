58385cffce7f4e87a1e77e5a8e38d5cf1f6e51c6
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2021-09-16 17:36:53 +0530

    dip

--- app/client/src/pages/common/CanvasArenas/CanvasSelectionArena.tsx
+++ app/client/src/pages/common/CanvasArenas/CanvasSelectionArena.tsx
@@ -126,7 +126,8 @@ export function CanvasSelectionArena({
       canDraw: canDrawOnEnter,
       startPoints: canDrawOnEnter ? outOfCanvasStartPositions : undefined,
     };
-    if (slidingArenaRef.current && canDrawOnEnter) {
+    if (slidingArenaRef.current && stickyCanvasRef.current && canDrawOnEnter) {
+      stickyCanvasRef.current.style.zIndex = "2";
       slidingArenaRef.current.style.zIndex = "2";
     }
   }, [
@@ -318,6 +319,7 @@ export function CanvasSelectionArena({
           isDragging = true;
           // bring the canvas to the top layer
           stickyCanvasRef.current.style.zIndex = "2";
+          slidingArenaRef.current.style.zIndex = "2";
         }
       };
 
@@ -340,6 +342,7 @@ export function CanvasSelectionArena({
             stickyCanvasRef.current.height,
           );
           stickyCanvasRef.current.style.zIndex = "";
+          slidingArenaRef.current.style.zIndex = "";
           dispatch(setCanvasSelectionStateAction(false, widgetId));
         }
       };
