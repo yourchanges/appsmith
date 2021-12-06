2e65c16eb3a35729c9824cfa8156de4eeb51763f
ohansFavour <fohanekwu@gmail.com>
2021-12-06 11:04:54 +0100

    customize datasource authorization

--- app/client/src/api/CloudServicesApi.ts
+++ app/client/src/api/CloudServicesApi.ts
@@ -2,5 +2,5 @@ import { getAppsmithConfigs } from "configs";
 
 const { cloudServicesBaseUrl: BASE_URL } = getAppsmithConfigs();
 
-export const authorizeSaasWithAppsmithToken = (appsmithToken: string) =>
+export const authorizeDatasourceWithAppsmithToken = (appsmithToken: string) =>
   `${BASE_URL}/api/v1/integrations/oauth/authorize?appsmithToken=${appsmithToken}`;
